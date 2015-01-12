e6c3013a4534b8c4da2d6948f1abbf16db1cd0fd
Howard Liberty <liberty@anymemo.org>
2015-01-12 11:45:20 -0800

    Add configuraiton to daydream length

--- src/main/java/org/liberty/android/burnindaydream/BurnInDream.java
+++ src/main/java/org/liberty/android/burnindaydream/BurnInDream.java
@@ -1,8 +1,11 @@
 package org.liberty.android.burnindaydream;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.service.dreams.DreamService;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -10,9 +13,12 @@ public class BurnInDream extends DreamService {
 
     private FrameLayout rootLayout;
 
-    private int currentColor = 0;
+    private int currentTick = 0;
 
     private Handler handler = new Handler();
+
+    private int maxTicks = 0;
+
     @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
@@ -33,25 +39,35 @@ public class BurnInDream extends DreamService {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
         rootLayout.setBackgroundColor(Color.GRAY);
         switchColors();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int limit = preferences.getInt("daydream_time_limit", 1200);
+        maxTicks = limit / 2;
     }
 
     public void switchColors() {
         handler.postDelayed(new Runnable() {
             public void run() {
-                currentColor++;
-                if (currentColor % 4 == 0) {
+                currentTick++;
+                if (currentTick % 4 == 0) {
                     rootLayout.setBackgroundColor(Color.GRAY);
                 }
-                if (currentColor % 4 == 1) {
+                if (currentTick % 4 == 1) {
                     rootLayout.setBackgroundColor(Color.RED);
                 }
-                if (currentColor % 4 == 2) {
+                if (currentTick % 4 == 2) {
                     rootLayout.setBackgroundColor(Color.GREEN);
                 }
-                if (currentColor % 4 == 3) {
+                if (currentTick % 4 == 3) {
                     rootLayout.setBackgroundColor(Color.BLUE);
                 }
-                switchColors();
+               //  Log.v("BurnInDream", "CurrentTick: " + currentTick + " max tick: " + maxTicks);
+                if (currentTick > maxTicks) {
+
+                    finish();
+                } else {
+                    switchColors();
+                }
             }
         }, 2000);
 
--- src/main/java/org/liberty/android/burnindaydream/MainActivity.java
+++ src/main/java/org/liberty/android/burnindaydream/MainActivity.java
@@ -1,13 +1,18 @@
 package org.liberty.android.burnindaydream;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
+    private EditText timeLimitEdit;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -29,5 +34,18 @@ public class MainActivity extends Activity
                 startActivity(new Intent(MainActivity.this, PreviewActivity.class));
             }
         });
+        timeLimitEdit = (EditText) findViewById(R.id.daydream_time_limit_edit);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int limit = preferences.getInt("daydream_time_limit", 1200);
+        timeLimitEdit.setText("" + limit);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        int limit = Integer.valueOf(timeLimitEdit.getText().toString());
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putInt("daydream_time_limit", limit);
+        editor.commit();
     }
 }
